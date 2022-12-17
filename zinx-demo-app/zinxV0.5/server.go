ee104502f50a9238f514c394f7cd81f7ea585af1
logeek <2031500185@qq.com>
2022-12-17 08:28:01 +0800

    V0.5.4: 应用了新的消息封装机制发送数据

---
+++ zinx-demo-app/zinxV0.5/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"zinx/ziface"
+	"zinx/znet"
+)
+
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+func (router *PingRouter) Handle(r ziface.IRequest) {
+	err := r.GetConnection().SendMsg(r.GetMsgId(), []byte("ping...ping...ping...\n"))
+	if err != nil {
+		fmt.Println("callback Handle error:", err)
+	}
+}
+
+func main() {
+	s := znet.NewServer()
+	s.AddRouter(&PingRouter{})
+	s.Serve()
+}
