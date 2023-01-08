3bb9a85036fb81771915e272ebd0746efe9a9a05
logeek <2031500185@qq.com>
2023-01-08 22:28:16 +0800

    V0.9.1: 将版本更新至0.9

--- zinx/utils/globalobj.go
+++ zinx/utils/globalobj.go
@@ -62,7 +62,7 @@ func init() {
 		Host:             "0.0.0.0",
 		TcpPort:          9999,
 		Name:             "ZinxServerApp",
-		Version:          "V0.8",
+		Version:          "V0.9",
 		MaxConn:          1000,
 		MaxPackageSize:   4096,
 		WorkerPoolSize:   0,
